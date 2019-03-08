3e2e528e7a956cbf4cb49a1b0a95e37fd3fe25e3
Michael Tauer <michael.tauer@futurice.com>
2019-03-08 16:04:41 +0100

    Tokenize a text according to known word vectors

--- word_vectors/word_vectors.py
+++ word_vectors/word_vectors.py
@@ -1,4 +1,11 @@
 import os.path
+import re
+
+LETTER_REGEX = re.compile(r'[a-zA-Z]')
+NON_LETTER_REGEX = re.compile(r'[^a-zA-Z]')
+DIGIT_REGEX = re.compile(r'[0-9]')
+NON_DIGIT_REGEX = re.compile(r'[^a-zA-Z]')
+LETTER_DIGIT_SPACE_REGEX = re.compile(r'[a-zA-Z0-9\s]');
 
 class WordVectors:
     def __init__(self, algorithm, dimensions):
@@ -11,25 +18,67 @@ class WordVectors:
         if self.algorithm == 'glove' and ensure_glove_files_exist():
             if len(self.sorted_words)>0:
                 return True
-
-            # read words from glove file and sort them by length
-            cur_index = 0;
+            # read words from glove text file and sort them by length
             with open('glove/glove.6B.' + str(self.dimensions) + 'd.txt') as f:
-                for line in f:
+                for i, line in enumerate(f):
                     word, *vector = line.split()
-                    self.sorted_words.append([word, cur_index])
+                    self.sorted_words.append([word, i])
                     self.vectors.append(list(map(lambda v: float(v), vector)))
-                    cur_index += 1
             self.sorted_words.sort(key=lambda x: len(x[0]), reverse=True)
+            print('Words initialized: ' + str(len(self.sorted_words)))
             return True
         return False
 
     def tokenize(self, text):
         if self.init():
-            # TODO tokenize properly
-            return text.split()
+            text = text.lower()
+            tokens = []
+            offset = 0
+            while len(text)>0:
+                word_with_index = self.get_first_word(text)
+                word_with_index.append(offset)
+                tokens.append(word_with_index)
+                word_length = len(word_with_index[0])
+                offset += word_length
+                text = text[word_length:]
+                if text.startswith(' '):
+                    text = text[1:]
+                    offset += 1
+            return tokens
         else:
             return []
+
+    def get_first_word(self, text):
+        for word_with_index in self.sorted_words:
+            if text == word_with_index[0]:
+                return word_with_index.copy()
+            if text.startswith(word_with_index[0]):
+                last_char_of_word = text[len(word_with_index[0]) - 1]
+                next_char = text[len(word_with_index[0])]
+                if LETTER_REGEX.search(last_char_of_word) and LETTER_REGEX.search(next_char):
+                    # A split between letters is not allowed. Return the word
+                    # until the next non-letter chararecter as an unknown word.
+                    return self.get_first_unknow_word(text, NON_LETTER_REGEX)
+                elif DIGIT_REGEX.search(last_char_of_word) and DIGIT_REGEX.search(next_char):
+                    # A split between digits is not allowed. Return the word
+                    # until the next non-digit chararecter as an unknown word.
+                    return self.get_first_unknow_word(text, NON_DIGIT_REGEX)
+                else:
+                    return word_with_index.copy()
+        # handle an unknown first words
+        first_char = text[0]
+        if LETTER_REGEX.search(first_char):
+            return self.get_first_unknow_word(text, NON_LETTER_REGEX)
+        elif DIGIT_REGEX.search(first_char):
+            return self.get_first_unknow_word(text, NON_DIGIT_REGEX)
+        else:
+            return self.get_first_unknow_word(text, LETTER_DIGIT_SPACE_REGEX)
+
+    def get_first_unknow_word(self, text, next_token_regex):
+        next_token_position = next_token_regex.search(text).start()
+        word = text[:next_token_position]
+        print('Word not found: ' + word)
+        return [word, -1]
 
 
 def ensure_glove_files_exist():
